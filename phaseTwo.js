cf6687183d8833f0f72777efbbed6118ff968458
Danny Zhou <dzhou005@ucr.edu>
2020-12-14 14:00:35 -0800

    First Project

---
+++ phaseTwo.js
@@ -0,0 +1,17 @@
+console.log("Hello Node!")
+console.log(5 - 4)
+console.log(7-3)
+console.log(9*8)
+console.log(4/2)
+let addTwo = function (num) {
+  return num + 2
+
+};
+
+console.log(addTwo(4))
+
+let helloNode = function (str) {
+  return "Hello Node"
+
+}
+console.log(helloNode())
