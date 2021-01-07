baacd66abf0559bea273682245315182a02a48ae
Savannah Rice <savannah.rice@gmail.com>
2021-01-07 11:04:11 -0700

    Added files

---
+++ storage-project.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+  const formFieldItems = document.getElementById('items');
+  const formFieldQuantity = document.getElementById('quantity');
+  const formSubmit = document.getElementById('add-to-cart');
+
+  formSubmit.addEventListener('click', (event) => {
+    
+
+
+  })
+
+  // const showCart = () => {
+
+  // };
+
+  // const storeItem = () => {
+
+  // };
+
+  // const removeItem = () => {
+
+  // };
+
+});
